39f372b0be4860e53e352cb27a9f04a2d5f46085
Pawan Saket <pawan@vujadelabs.com>
2020-12-12 23:51:17 +0530

    Adding first files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Components/Checkout/Checkout";
+import Login from "./Components/Login/Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    //Will only run once when the app component loads....
+    auth.onAuthStateChanged((authUser) => {
+      console.log(`THe user is`, authUser);
+      if (authUser) {
+        //the use just logged in / the was user logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //BEM convention
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import "./Login.css";
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "../../firebase";
+
+function Login() {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = (e) => {
+    e.preventDefault();
+    //Some fancy firebase login shitttt......
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((signedIn) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
+  };
+  const register = (e) => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authen) => {
+        console.log("AUthe success==", authen);
+        if (authen) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+    //Some fancy firebase register shitt....
+  };
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt="login_logo"
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
+        </form>
+        <p>
+          By signing-in you agree to the AMAZON CLONE <a href="#">Conditions</a>{" "}
+          of Use & Sale. Please see our Privacy Notice, our Cookies{" "}
+          <a href="#">Notice</a> and our <a href="#">Interest-Based Ads</a>{" "}
+          Notice.
+        </p>
+        <button onClick={register} className="login__registerButton">
+          Create your Amazon account
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
